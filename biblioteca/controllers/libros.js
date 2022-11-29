588c7ce9af38b15b56d40d94b8ea930de171ba08
Majo <majoaraujopiedra@outlook.com>
2022-11-29 20:54:05 -0300

    delete

--- biblioteca/controllers/libros.js
+++ biblioteca/controllers/libros.js
@@ -32,25 +32,48 @@ router.post('/', async (req, res) => {
 })
 
 router.put('/:id', (req,res) => { 
-    Task.update({ 
-        usuarioId: req.body.usuarioId,
-        nombre: req.body.nombre,
-        categoria: req.body.categoria,
-        autor: req.body.autor,
-        fechaInPrestamo: req.body.fechaInPrestamo,
-        fechaFinPrestamo: req.body.fechaFinPrestamo }, {
-        where: {
-            usuarioId: Libros.usuarioId,
-            nombre: Libros.nombre,
-            categoria: Libros.categoria,
-            autor: Libros.autor,
-            fechaInPrestamo: Libros.fechaInPrestamo,
-            fechaFinPrestamo: Libros.fechaFinPrestamo
-        }
-        .catch(err => {
-            res.status(500).send(err);
-    })
+        Task.update({ 
+            usuarioId: req.body.usuarioId,
+            nombre: req.body.nombre,
+            categoria: req.body.categoria,
+            autor: req.body.autor,
+            fechaInPrestamo: req.body.fechaInPrestamo,
+            fechaFinPrestamo: req.body.fechaFinPrestamo }, {
+            where: {
+                usuarioId: Libros.usuarioId,
+                nombre: Libros.nombre,
+                categoria: Libros.categoria,
+                autor: Libros.autor,
+                fechaInPrestamo: Libros.fechaInPrestamo,
+                fechaFinPrestamo: Libros.fechaFinPrestamo
+            }
+            .catch(err => {
+                res.status(500).send(err);
+        })
     })
 })
+router.delete('/:id', (req,res) => {
+    const id = req.params.id;
+      Libros.destroy({
+          where: {
+            id:id
+          }
+        })
+        .then(num => {
+          if (num == 1) {
+            res.send({
+              message: "El libro se eliminó con éxito!"
+            });
+          }else if(num == 0){
+            res.status(404).send({
+              message: "No se ha encontrado el libro a eliminar"
+            });  
+          }
+      })
+        .catch(err => {
+          res.status(500).send(err);
+      });
+  })
+  
 
 module.exports = router
